3998d430384bd9ca6f9159101208030ee799fc38
Zohanubis <phdh141003@gmail.com>
2023-09-19 00:40:34 +0700

    19/9

---
+++ Lesson5/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRandomName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnXoaPhanTuDaChon_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnXoaPhanTuTenSon_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnXoaPTHoLe_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnChuyenChuoiInHoa_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnChuyenChuoiInThuong_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnChuyenChuoiInHoaODauTu_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnXoaTatCaPT_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
