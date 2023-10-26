2e18c410d826e9f9f90f010d648e3c521ef645fd
Zohanubis <phdh141003@gmail.com>
2023-10-26 14:59:45 +0700

    26/10

--- Lesson11/Sample_Ex1/Sample_Ex1.Designer.cs
+++ Lesson11/Sample_Ex1/Sample_Ex1.Designer.cs
@@ -42,8 +42,10 @@ namespace Sample_Ex1
             this.txtTenSV = new System.Windows.Forms.TextBox();
             this.maskedTextBoxDate = new System.Windows.Forms.MaskedTextBox();
             this.comboBoxLop = new System.Windows.Forms.ComboBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.tableLayoutPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnAdd
@@ -56,6 +58,7 @@ namespace Sample_Ex1
             this.btnAdd.TabIndex = 0;
             this.btnAdd.Text = "Thêm";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnDelete
             // 
@@ -67,6 +70,7 @@ namespace Sample_Ex1
             this.btnDelete.TabIndex = 1;
             this.btnDelete.Text = "Xóa";
             this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // btnEdit
             // 
@@ -78,6 +82,7 @@ namespace Sample_Ex1
             this.btnEdit.TabIndex = 2;
             this.btnEdit.Text = "Sửa";
             this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
             // 
             // btnSave
             // 
@@ -89,6 +94,7 @@ namespace Sample_Ex1
             this.btnSave.TabIndex = 3;
             this.btnSave.Text = "Lưu";
             this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // btnExit
             // 
@@ -100,14 +106,15 @@ namespace Sample_Ex1
             this.btnExit.TabIndex = 4;
             this.btnExit.Text = "Đóng";
             this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // lableMaSV
             // 
             this.lableMaSV.AutoSize = true;
             this.lableMaSV.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lableMaSV.Location = new System.Drawing.Point(129, 108);
+            this.lableMaSV.Location = new System.Drawing.Point(3, 0);
             this.lableMaSV.Name = "lableMaSV";
-            this.lableMaSV.Size = new System.Drawing.Size(115, 20);
+            this.lableMaSV.Size = new System.Drawing.Size(79, 40);
             this.lableMaSV.TabIndex = 5;
             this.lableMaSV.Text = "Mã Sinh Viên";
             // 
@@ -115,7 +122,7 @@ namespace Sample_Ex1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(129, 173);
+            this.label1.Location = new System.Drawing.Point(3, 55);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(90, 20);
             this.label1.TabIndex = 6;
@@ -125,7 +132,7 @@ namespace Sample_Ex1
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(565, 108);
+            this.label2.Location = new System.Drawing.Point(378, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(121, 20);
             this.label2.TabIndex = 7;
@@ -135,7 +142,7 @@ namespace Sample_Ex1
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(565, 173);
+            this.label3.Location = new System.Drawing.Point(378, 55);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(39, 20);
             this.label3.TabIndex = 8;
@@ -143,21 +150,22 @@ namespace Sample_Ex1
             // 
             // txtMaSV
             // 
-            this.txtMaSV.Location = new System.Drawing.Point(291, 108);
+            this.txtMaSV.Location = new System.Drawing.Point(111, 3);
             this.txtMaSV.Name = "txtMaSV";
             this.txtMaSV.Size = new System.Drawing.Size(232, 20);
             this.txtMaSV.TabIndex = 9;
             // 
             // txtTenSV
             // 
-            this.txtTenSV.Location = new System.Drawing.Point(714, 108);
+            this.txtTenSV.Location = new System.Drawing.Point(530, 3);
             this.txtTenSV.Name = "txtTenSV";
             this.txtTenSV.Size = new System.Drawing.Size(243, 20);
             this.txtTenSV.TabIndex = 10;
             // 
             // maskedTextBoxDate
             // 
-            this.maskedTextBoxDate.Location = new System.Drawing.Point(291, 173);
+            this.maskedTextBoxDate.Location = new System.Drawing.Point(111, 58);
+            this.maskedTextBoxDate.Mask = "00/00/0000";
             this.maskedTextBoxDate.Name = "maskedTextBoxDate";
             this.maskedTextBoxDate.Size = new System.Drawing.Size(232, 20);
             this.maskedTextBoxDate.TabIndex = 11;
@@ -165,42 +173,62 @@ namespace Sample_Ex1
             // comboBoxLop
             // 
             this.comboBoxLop.FormattingEnabled = true;
-            this.comboBoxLop.Location = new System.Drawing.Point(714, 172);
+            this.comboBoxLop.Location = new System.Drawing.Point(530, 58);
             this.comboBoxLop.Name = "comboBoxLop";
             this.comboBoxLop.Size = new System.Drawing.Size(243, 21);
             this.comboBoxLop.TabIndex = 12;
             // 
-            // dataGridView1
+            // dataGridView
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(133, 226);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(824, 201);
-            this.dataGridView1.TabIndex = 13;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(133, 226);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(824, 201);
+            this.dataGridView.TabIndex = 13;
+            this.dataGridView.SelectionChanged += new System.EventHandler(this.dataGridView_SelectionChanged);
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.ColumnCount = 5;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.51282F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.66413F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 28.8425F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 295F));
+            this.tableLayoutPanel.Controls.Add(this.lableMaSV, 0, 0);
+            this.tableLayoutPanel.Controls.Add(this.txtMaSV, 1, 0);
+            this.tableLayoutPanel.Controls.Add(this.comboBoxLop, 4, 1);
+            this.tableLayoutPanel.Controls.Add(this.label1, 0, 1);
+            this.tableLayoutPanel.Controls.Add(this.txtTenSV, 4, 0);
+            this.tableLayoutPanel.Controls.Add(this.maskedTextBoxDate, 1, 1);
+            this.tableLayoutPanel.Controls.Add(this.label3, 2, 1);
+            this.tableLayoutPanel.Controls.Add(this.label2, 2, 0);
+            this.tableLayoutPanel.Location = new System.Drawing.Point(133, 97);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 2;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(824, 111);
+            this.tableLayoutPanel.TabIndex = 14;
             // 
             // Sample_Ex1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1080, 510);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.comboBoxLop);
-            this.Controls.Add(this.maskedTextBoxDate);
-            this.Controls.Add(this.txtTenSV);
-            this.Controls.Add(this.txtMaSV);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.lableMaSV);
+            this.Controls.Add(this.tableLayoutPanel);
+            this.Controls.Add(this.dataGridView);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.btnEdit);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnAdd);
             this.Name = "Sample_Ex1";
-            this.Text = "Form1";
+            this.Text = "Bài Tập Mãu 1";
             this.Load += new System.EventHandler(this.Sample_Ex1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.tableLayoutPanel.ResumeLayout(false);
+            this.tableLayoutPanel.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -221,7 +249,8 @@ namespace Sample_Ex1
         private System.Windows.Forms.TextBox txtTenSV;
         private System.Windows.Forms.MaskedTextBox maskedTextBoxDate;
         private System.Windows.Forms.ComboBox comboBoxLop;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
     }
 }
 
